4a36ea567f4d51626cde16748edc2f34b9076067
Quốc Cường <150774639+cuongdola0@users.noreply.github.com>
2024-08-28 10:21:42 +0700

    Maxmin.java

--- maxmin/src/maxmin/Maxmin.java
+++ maxmin/src/maxmin/Maxmin.java
@@ -1,20 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package maxmin;
+import java.util.Scanner;
 
-/**
- *
- * @author LENOVO
- */
 public class Maxmin {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập vào số phần tử của mảng: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        
+        System.out.println("Nhập vào các phần tử của mảng:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        int max = array[0];
+        int min = array[0];
+        
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        
+        System.out.println("Giá trị lớn nhất: " + max);
+        System.out.println("Giá trị nhỏ nhất: " + min);
+        scanner.close();
     }
-    
 }
